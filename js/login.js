537c946564e2032206fddb94969f47638b68500d
shanto <mdnazmulhossenshanto01@gmail.com>
2025-03-09 10:14:53 +0600

    added login form

--- js/login.js
+++ js/login.js
@@ -1,5 +1,12 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    if(phoneNumber === '0130' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html'
+    }
+    else{
+        console.log('wrong phone number or pin');
+    }
 })
